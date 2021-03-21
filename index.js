ab4b9a26092cfed93476b02a10f7c583276c90e4
Ebenezer Don <ebenezerdonu@gmail.com>
2021-03-21 16:53:11 +0100

    add getPhotoUrl function

---
+++ index.js
@@ -0,0 +1,16 @@
+const getPhotoUrl = selector => {
+  const photoInput = document.querySelector(selector)
+
+  const photoUrl = new Promise((resolve, reject) => {
+    photoInput.addEventListener('change', () => {
+      const reader = new FileReader()
+      reader.readAsDataURL(photoInput.files[0])
+
+      reader.addEventListener('load', () => {
+        resolve(reader.result)
+      })
+    })
+  })
+
+  return Promise.resolve(photoUrl)
+}
